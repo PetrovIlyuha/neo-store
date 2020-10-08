2d1ef5f8a037d1a659f555b5f91a8ef15ce0cb68
PetrovIlyuha <dnamix1@gmail.com>
2020-10-08 15:41:02 +0300

    added routes related to user's auth flow operations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
@@ -7,6 +7,10 @@ import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import NotFound from "./components/UIState/NotFound";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const App = () => {
   return (
@@ -14,9 +18,15 @@ const App = () => {
       <Header />
       <main className='py-4'>
         <Container>
-          <Route path='/' exact component={HomeScreen} />
-          <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
+          <Switch>
+            <Route path='/' exact component={HomeScreen} />
+            <Route path='/product/:id' component={ProductScreen} />
+            <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/profile' component={ProfileScreen} />
+            <Route path='/login' component={LoginScreen} />
+            <Route path='/register' component={RegisterScreen} />
+            <Route path='*' component={NotFound} />
+          </Switch>
         </Container>
       </main>
       <Footer />
