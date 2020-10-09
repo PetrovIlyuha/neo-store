fbe1c8d3df4bfbfeba323344034af8fee6b5ec4a
PetrovIlyuha <dnamix1@gmail.com>
2020-10-09 11:18:17 +0300

    added user-related routes to express server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import colors from "colors"
-import connectDB from "./config/db.js"
-import productRoutes from "./routes/productRoutes.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
-connectDB()
-const app = express()
+dotenv.config();
+connectDB();
+const app = express();
 
-app.use("/api/products/", productRoutes)
+app.use(express.json());
 
-app.use(notFound)
-app.use(errorHandler)
+app.use("/api/products/", productRoutes);
+app.use("/api/users/", userRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
     `Server spinning in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
       .yellow.bold
-  )
-})
+  );
+});
