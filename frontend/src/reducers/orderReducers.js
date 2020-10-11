f7d81001f58a0a16736cff1893ea433dc3180e8b
PetrovIlyuha <dnamix1@gmail.com>
2020-10-11 11:50:00 +0300

    reducer for order creation added

---
+++ frontend/src/reducers/orderReducers.js
@@ -0,0 +1,18 @@
+import {
+  ORDER_CREATE_FAILURE,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+} from "../constants/orderConstants";
+
+export const orderCreateReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ORDER_CREATE_REQUEST:
+      return { ...state, loading: true };
+    case ORDER_CREATE_SUCCESS:
+      return { ...state, loading: false, success: true, order: payload };
+    case ORDER_CREATE_FAILURE:
+      return { ...state, loading: false, error: payload };
+    default:
+      return state;
+  }
+};
