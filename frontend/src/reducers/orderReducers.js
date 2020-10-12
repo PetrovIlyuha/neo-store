c0e6b6fded1b238b14d5733b3e5de39a3e65f371
PetrovIlyuha <dnamix1@gmail.com>
2020-10-12 15:02:49 +0300

    added order details reducer function

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_FAILURE,
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAILURE,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, { type, payload }) => {
@@ -12,6 +15,22 @@ export const orderCreateReducer = (state = {}, { type, payload }) => {
       return { ...state, loading: false, success: true, order: payload };
     case ORDER_CREATE_FAILURE:
       return { ...state, loading: false, error: payload };
+    default:
+      return state;
+  }
+};
+
+export const orderDetailsReducer = (
+  state = { loading: true, orderItems: [], shippingAddress: {} },
+  { type, payload },
+) => {
+  switch (type) {
+    case ORDER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case ORDER_DETAILS_SUCCESS:
+      return { ...state, loading: false, order: payload };
+    case ORDER_DETAILS_FAILURE:
+      return { ...state, error: payload };
     default:
       return state;
   }
