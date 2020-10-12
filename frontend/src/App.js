576bbd4da1aae653f76e046eb50e7910a4604b88
PetrovIlyuha <dnamix1@gmail.com>
2020-10-12 15:01:41 +0300

    added route for order page component to main App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import Placement from "./screens/Placement";
+import OrderScreen from "./screens/OrderScreen";
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
             <Route path='/shipping' component={ShippingScreen} />
             <Route path='/payment' component={PaymentScreen} />
             <Route path='/placeorder' component={Placement} />
+            <Route path='/order/:id' component={OrderScreen} />
             <Route path='*' component={NotFound} />
           </Switch>
         </Container>
