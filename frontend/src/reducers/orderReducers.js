6730238f439088ab34c29596900a5ae15cb819c9
PetrovIlyuha <dnamix1@gmail.com>
2020-10-13 11:27:36 +0300

    redux reducers for order payment and getting orders by user

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,13 @@ import {
   ORDER_DETAILS_FAILURE,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_FAILURE,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_RESET,
+  ORDER_PAY_SUCCESS,
+  ORDER_MY_ORDERS_FAILURE,
+  ORDER_MY_ORDERS_REQUEST,
+  ORDER_MY_ORDERS_SUCCESS,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, { type, payload }) => {
@@ -31,6 +38,48 @@ export const orderDetailsReducer = (
       return { ...state, loading: false, order: payload };
     case ORDER_DETAILS_FAILURE:
       return { ...state, error: payload };
+    default:
+      return state;
+  }
+};
+
+export const orderPayReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ORDER_PAY_REQUEST:
+      return { ...state, loading: true };
+    case ORDER_PAY_SUCCESS:
+      return { ...state, loading: false, success: true };
+    case ORDER_PAY_FAILURE:
+      return { ...state, error: payload };
+    case ORDER_PAY_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const orderMyOrdersReducer = (
+  state = { orders: [] },
+  { type, payload },
+) => {
+  switch (type) {
+    case ORDER_MY_ORDERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ORDER_MY_ORDERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        orders: payload,
+      };
+    case ORDER_MY_ORDERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
     default:
       return state;
   }
