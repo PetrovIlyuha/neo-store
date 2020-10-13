7490f1efe3d152cc57ad50ea3d45c300230d27ce
PetrovIlyuha <dnamix1@gmail.com>
2020-10-13 11:30:55 +0300

    order controller methods (update order to paid status / get orders for current user)

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,8 @@
 import asyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
+import mongoose from "mongoose";
+import User from "../models/userModel.js";
+const ObjectId = mongoose.Types.ObjectId;
 
 // * @desc Create new order
 // ? @route POST /api/orders
@@ -54,4 +57,34 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// * @desc Update order to paid status
+// ? @route PUT /api/orders/:id/pay
+// ! @access Protected
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// * @desc Get currently logged in user's orders
+// ? @route GET /api/orders/myorders
+// ! @access Protected
+const ordersByUser = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id.toString() });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, ordersByUser };
