b62d5660cb7fb618ed4a035de676270c64235d61
PetrovIlyuha <dnamix1@gmail.com>
2020-10-15 11:46:43 +0300

    get users redux async action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -14,6 +14,9 @@ import {
   USER_UPDATE_PROFILE_FAILURE,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAILURE,
 } from "../constants/userConstants";
 import { ORDER_MY_ORDERS_RESET } from "../constants/orderConstants";
 export const login = (email, password) => async dispatch => {
@@ -125,6 +128,31 @@ export const updateUserProfile = user => async (dispatch, getState) => {
   } catch (err) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAILURE,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_LIST_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(`/api/users`, config);
+    dispatch({ type: USER_LIST_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: USER_LIST_FAILURE,
       payload:
         err.response && err.response.data.message
           ? err.response.data.message
