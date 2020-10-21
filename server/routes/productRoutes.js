94b3f90b3ded47065844a7ab7fd6c171d3532be9
PetrovIlyuha <dnamix1@gmail.com>
2020-10-21 11:25:18 +0300

    added product CRUD routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts);
 router.route("/:id").get(getProductById);
-
+router.route("/:id").delete(protect, admin, deleteProduct);
+router.route("/:id").put(protect, admin, updateProduct);
+router.route("/").post(protect, admin, createProduct);
 export default router;
