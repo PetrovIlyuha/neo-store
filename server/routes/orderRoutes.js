cb9e4aaa0dcfde693bcab9e0874a6950f7dc6c84
PetrovIlyuha <dnamix1@gmail.com>
2020-10-22 12:27:41 +0300

    route (express) for changing order delivery status to delivered

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,16 +1,20 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 import {
   addOrderItems,
+  getAllOrders,
   getOrderById,
   getUserOrders,
+  updateOrderToDelivered,
   updateOrderToPaid,
-} from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+} from '../controllers/orderController.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
-router.route("/myorders").get(protect, getUserOrders);
-router.route("/").post(protect, addOrderItems);
-router.route("/:id").get(protect, getOrderById);
-router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route('/myorders').get(protect, getUserOrders);
+router.route('/').get(protect, admin, getAllOrders);
+router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/delivered').put(protect, admin, updateOrderToDelivered);
 
 export default router;
