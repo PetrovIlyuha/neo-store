1f70fd0ce630f7c86cdba312cca664954888e302
PetrovIlyuha <dnamix1@gmail.com>
2020-10-25 12:17:55 +0300

    product route added to router for top rated products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,10 +7,12 @@ import {
   deleteProduct,
   updateProduct,
   createProductReview,
+  getTopRatedProducts,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
+router.route('/top').get(getTopRatedProducts);
 router.route('/:id').get(getProductById);
 router.route('/:id').delete(protect, admin, deleteProduct);
 router.route('/:id').put(protect, admin, updateProduct);
